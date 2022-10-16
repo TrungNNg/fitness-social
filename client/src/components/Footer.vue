c27aaf06bc641abed0cb4743f8b0436bf1d0f888
Trung Nguyen <ngtrung0916@gmail.com>
2022-10-16 11:47:45 -0400

    style footer and 404 page

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -3,11 +3,13 @@
 </script>
 
 <template> 
-    <footer class="footer">
-  <div class="content has-text-centered">
-    <p>
-      created by Trung Ng
-    </p>
-  </div>
-</footer>
+  <footer class="footer">
+    <div class="has-text-centered">
+      <h3>Follow Us</h3>
+      <span class="icon">
+        <a href="https://facebook.com"><i class="fa fa-facebook">Facebook&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i></a>
+        <a href="https://twitter.com"><i class="fa fa-twitter">Twitter</i></a>
+      </span>
+    </div>
+  </footer>
 </template>
--- client/src/views/404View.vue
+++ client/src/views/404View.vue
@@ -3,6 +3,18 @@
 </script>
 
 <template> 
-    <p>404 Page not found</p>
+    <section class="section is-medium">
+    <div class="container">
+      <div class="columns is-vcentered">
+        <div class="column has-text-centered">
+          <h1 class="title">404 Page Not Found</h1>
+          <p class="subtitle">Look like what you looking for is not here.</p>
+        </div>
+        <div class="column has-text-centered">
+          <img src="../assets/404.jpg" />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
