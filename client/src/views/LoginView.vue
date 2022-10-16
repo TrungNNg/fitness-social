c25eeb3a36f0afef237fc63f077a12bb29a85940
Trung Nguyen <ngtrung0916@gmail.com>
2022-10-16 13:54:29 -0400

    style signin & signup form

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -3,5 +3,28 @@
 </script>
 
 <template> 
-    <p>Login View</p>
+   <br/>
+    <br/>
+    <div class="column is-4 is-offset-4">
+        <div class="title">Login</div>
+        <div class="field">
+            <label class="label">username</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="username">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label">password</label>
+            <div class="control">
+                <input class="input" type="password" placeholder="password">
+            </div>
+        </div>
+        <div>
+            <button class="button is-link">submit</button>
+        </div>
+        <br/>
+        <br/>
+        <a href="">forgot password?</a>
+    </div>
 </template>
--- client/src/views/SignUpView.vue
+++ client/src/views/SignUpView.vue
@@ -3,5 +3,34 @@
 </script>
 
 <template> 
-    <p>SIGN UP VIEW</p>
+    <br/>
+    <br/>
+    <div class="column is-4 is-offset-4">
+        <div class="title">Sign Up</div>
+        <div class="field">
+            <label class="label">username</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="username">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label">email</label>
+            <div class="control">
+                <input class="input" type="email" placeholder="email">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label">password</label>
+            <div class="control">
+                <input class="input" type="password" placeholder="password">
+            </div>
+        </div>
+        <div>
+            <button class="button is-link">submit</button>
+        </div>
+        <br/>
+        <br/>
+    </div>
 </template>
