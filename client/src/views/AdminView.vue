33a0f709c442e3bb203592c1ea1425e5195413cf
Trung Nguyen <ngtrung0916@gmail.com>
2022-10-20 20:55:32 -0400

    fix type check for adminview

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -4,21 +4,20 @@
   import session from '../stores/session.js';
   import router from '../router';
 
-  let users = []
+  const users = reactive(data)
+  console.log(typeof data)
 
   // let say bob is admin
   session.user = {username:"Bob", index:0}
 
   if (session.user?.username !== "Bob") {
     router.push({name:"home"})
-  } else {
-    users = reactive(data)
   }
 
   function remove(name:String) {
     let index = 0
     for (let i = 0; i < users.length; i++) {
-      if (name === users[i]) {
+      if (name === users[i].username) {
         index = i
         break
       }
@@ -31,7 +30,7 @@
     <div>Admin Page, let say Bob is admin</div>
     <br/>
     <div>user list</div>
-    <div v-for="u in users" @click="remove(u)">click to remove users {{u.username}}</div>
+    <div v-for="u in users" @click="remove(u.username)">click to remove users {{u.username}}</div>
     <br/>
     <div>user also remove in mock database {{data}}</div>
    
