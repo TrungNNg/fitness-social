5e4c267338321631c8a69f8b23fb0b170ae34082
Trung Nguyen <ngtrung0916@gmail.com>
2022-12-05 20:37:42 -0500

    fix type error

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -4,34 +4,10 @@
   import session from '../stores/session.js';
   import router from '../router';
 
-  const users = reactive(data)
-  console.log(typeof data)
-
-  // let say bob is admin
-  session.user = {username:"Bob", index:0}
-
-  if (session.user.username !== "Bob") {
-    router.push({name:"home"})
-  }
-
-  function remove(name:String) {
-    let index = 0
-    for (let i = 0; i < users.length; i++) {
-      if (name === users[i].username) {
-        index = i
-        break
-      }
-    }
-    users.splice(index, 1)
-  }
 </script>
 
 <template> 
     <div>Admin Page, let say Bob is admin</div>
-    <br/>
-    <div>user list</div>
-    <div v-for="u in users" @click="remove(u.username)">click to remove users {{u.username}}</div>
-    <br/>
-    <div>user also remove in mock database {{data}}</div>
+    
    
 </template>
